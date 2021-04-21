103a3944066042d5146ee0c01b18ff5562148f53
notGonim <mhmodgonim96@gmail.com>
2021-04-21 10:37:51 +0200

    ADDED data to the table component

--- src/app/collections/collection-home/collection-home.component.ts
+++ src/app/collections/collection-home/collection-home.component.ts
@@ -3,13 +3,43 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-collection-home',
   templateUrl: './collection-home.component.html',
-  styleUrls: ['./collection-home.component.css']
+  styleUrls: ['./collection-home.component.css'],
 })
 export class CollectionHomeComponent implements OnInit {
+  data: { name: string; age: number; job: string }[] = [
+    {
+      name: 'James',
+      age: 25,
+      job: 'Designer',
+    },
+    {
+      name: 'mhmod',
+      age: 24,
+      job: 'Developer',
+    },
+    {
+      name: 'Esraa',
+      age: 21,
+      job: 'Marketing',
+    },
+  ];
 
-  constructor() { }
+  header: { key: string; label: string }[] = [
+    {
+      key: 'name',
+      label: 'Name',
+    },
+    {
+      key: 'age',
+      label: 'Age',
+    },
+    {
+      key: 'job',
+      label: 'Job',
+    },
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
