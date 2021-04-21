8116edfe149ddb6fe23435c6ef99a534cf9aa5b3
notGonim <mhmodgonim96@gmail.com>
2021-04-21 08:10:11 +0200

    Initialize the main structure and modules

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { CollectionsModule } from './collections/collections.module';
+import { ElementsModule } from './elements/elements.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ElementsModule, CollectionsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
