199d615d1b3325f519bac61fb052c0301120b8a2
notGonim <mhmodgonim96@gmail.com>
2021-04-22 12:39:35 +0200

    ADDED the views module

---
+++ src/app/views/stastics/stastics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stastics',
+  templateUrl: './stastics.component.html',
+  styleUrls: ['./stastics.component.css']
+})
+export class StasticsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ViewsRoutingModule } from './views-routing.module';
+import { ViewHomeComponent } from './view-home/view-home.component';
+import { StasticsComponent } from './stastics/stastics.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ViewHomeComponent,
+    StasticsComponent
+  ],
   imports: [
     CommonModule,
     ViewsRoutingModule
